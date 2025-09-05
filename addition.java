3da0fc7ae080507b6024fdc5f050315b75172096
vijayyadav <vijay.9871yadav@gmail.com>
2025-09-05 22:56:26 +0530

    git commit and push after change in addition

--- addition.java
+++ addition.java
@@ -1,4 +1,13 @@
 public class addition {
+
+
+/**
+     * Adds two integers and returns the result.
+     *
+     * @param a the first integer
+     * @param b the second integer
+     * @return the sum of a and b
+     */
     public static int add(int a, int b) {
         return a + b;
     }
