38994f074ba529bee27986a41c2d608de86ddbb9
vijayyadav <vijay.9871yadav@gmail.com>
2025-09-05 23:01:06 +0530

    git commit and push after refractoring

--- addition.java
+++ addition.java
@@ -1,13 +1,7 @@
 public class addition {
 
 
-/**
-     * Adds two integers and returns the result.
-     *
-     * @param a the first integer
-     * @param b the second integer
-     * @return the sum of a and b
-     */
+
     public static int add(int a, int b) {
         return a + b;
     }
